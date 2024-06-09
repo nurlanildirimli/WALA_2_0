940716ace5c1384c59be54ee7f71f469bbe42a03
nurlanildirimli <79944210+nurlanildirimli@users.noreply.github.com>
2024-06-10 00:36:16 +0400

    Update crawler.py

--- Extension/Backend/crawler.py
+++ Extension/Backend/crawler.py
@@ -1,28 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-# from urllib.parse import urljoin, urlparse
-#
-#
-# def get_base_url(url):
-#     parsed_url = urlparse(url)
-#     base_url = parsed_url.scheme + "://" + parsed_url.netloc
-#     return base_url
-#
-#
-# def crawler(url):
-#     inner_urls = set()
-#     base_url = get_base_url(url)
-#     response = requests.get(url)  # Getting URLs from here
-#     parse = BeautifulSoup(response.text, 'html.parser')  # Using BS4 for parsing
-#     for link in parse.find_all('a', href=True):  # Getting a tags with href and traversing them
-#         inner_url = link.get('href')  # Get href attribute which contains URL actually
-#         inner_url = urljoin(url, inner_url)  # Join the inner URL with the base URL
-#         if inner_url.startswith(
-#                 base_url) and inner_url != url and inner_url != base_url and '#' not in inner_url:  # Check if the URL starts with the base URL
-#             inner_urls.add(inner_url)  # Put into list
-#
-#     return list(inner_urls)
-
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
@@ -37,7 +12,9 @@ def get_base_url(url):
 def crawler(url):
     inner_urls = set()
     base_url = get_base_url(url)
-    response = requests.get(url)  # Getting URLs from here
+    response = requests.get(url, headers={"User-Agent":"XY"})  # Getting URLs from here we used headers to not getting 406 error. And not verifying because of SSLcertificate problems.
+    if(response.status_code != 200): response = requests.get(url, headers={"User-Agent":"XY"}, verify=False)
+
     parse = BeautifulSoup(response.text, 'html.parser')  # Using BS4 for parsing
     for link in parse.find_all('a', href=True):  # Getting a tags with href and traversing them
         inner_url = link.get('href')  # Get href attribute which contains URL actually
@@ -45,8 +22,5 @@ def crawler(url):
         if inner_url.startswith(
                 base_url) and inner_url != url and inner_url != base_url and '#' not in inner_url:  # Check if the URL starts with the base URL
             inner_urls.add(inner_url)  # Put into list
-            if len(inner_urls) == 2:  # Stop collecting inner URLs after obtaining the first two
-                break
 
     return list(inner_urls)
-
